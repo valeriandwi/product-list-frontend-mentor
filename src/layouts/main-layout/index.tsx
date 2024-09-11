a9110edd290ee9fec183e9d97045dc387cd2931e
Valerian <valerian.purnomo@inticorp.tech>
2024-09-11 17:25:56 +0700

    Improve layout

--- src/layouts/main-layout/index.tsx
+++ src/layouts/main-layout/index.tsx
@@ -6,7 +6,7 @@ interface MainLayoutProps {
 
 const MainLayout: React.FC<MainLayoutProps> = ({ children }) => {
   return (
-    <div className="py-[74px] px-[92px] flex justify-center bg-rose-50 max-md:px-4 max-md:py-4">
+    <div className="py-[74px] px-[92px] min-h-screen flex justify-center bg-rose-50 max-md:px-4 max-md:py-4">
       {children}
     </div>
   );
